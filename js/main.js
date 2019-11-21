3c38611c8f579491e093de51d74f991350bc488a
Arnoldas <support@coincrate.net>
2019-11-21 13:35:21 +0200

    empty

---
+++ js/main.js
@@ -0,0 +1,22 @@
+const addDivs = document.querySelector('.container');
+
+
+
+    for (let i=1; i<=16; i++) {
+        const block = document.createElement('div');
+        document.body.appendChild(block);
+        block.textContent = i;
+    }
+    function random(number) {
+        return Math.floor(Math.random()*number);
+    }
+    function generate() {
+        let rndCol = 'rbg(' + random(255) + ',' + random(255) + ',' + random(255) + ')';
+        return rndCol;
+    }
+        let divs = document.querySelectorAll('div');
+        for (let i=0; i<divs.length; i++){
+            divs[i].onclick = function (e) {
+                e.target.style.backgroundColor = generate();
+            }
+        }
